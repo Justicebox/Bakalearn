fd9ca265c3d9f78bcc2d3534f598f316b6e643a3
justicebox <justicebox@hotmail.com>
2019-11-13 15:09:46 +0800

    register

---
+++ vue_register/gjgj/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import register from '../views/register.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/', //把主页面改为register
+        name: 'register',
+        component: register
+    },
+    {
+        path: '/Home',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
